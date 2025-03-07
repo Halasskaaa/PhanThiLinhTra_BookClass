f4df4308beb2555cfb8c225634dd79e4e79ae741
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-03-07 12:24:00 +0100

    Egy paraméteres konstruktor kész

--- osztalyok_gyak/konyvtar.cs
+++ osztalyok_gyak/konyvtar.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace osztalyok_gyak
 {
-	internal class Konyvtar
+	internal class Book
 	{
+		string title;
+		string author;
+		int pageCount;
+		int releaseYear;
+
+		public Book(string title, string author, int pageCount, int releaseYear)
+		{
+			this.title = title;
+			this.author = author;
+			this.pageCount = pageCount;
+			this.releaseYear = releaseYear;
+		}
+
+		public string Title { get => title; set => title = value; }
+		public string Author { get => author; set => author = value; }
+		public int PageCount { get => pageCount; set => pageCount = value; }
+		public int ReleaseYear { get => releaseYear; set => releaseYear = value; }
+
+
 	}
 }
