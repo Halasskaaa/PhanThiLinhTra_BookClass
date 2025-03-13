2368ffbfd614ad6789741e000b5b8359f5e18270
Halasskaaa <phanthilinhtra25@gmail.com>
2025-03-13 22:29:37 +0100

    Update Program.cs
    fixed Book array

--- osztalyok_gyak/Program.cs
+++ osztalyok_gyak/Program.cs
@@ -4,16 +4,13 @@
 	{
 		static void Main(string[] args)
 		{
+			Book book1 = new Book("Harbinger of Chaos: Ruination", "Amy Stake", 911, 2001);
+			Book book2 = new Book("Lucy was in Fact a Lucifer", "Paige Turner", 666, 1313);
+			Book book3 = new Book("Shack Pallett at 5", "Jenny Rator", 614, 2016);
+			Book book4 = new Book("How Backflips are Relevant during Fight Scenes", "Leon S. Kennedy", 322, 1996);
+			Book book5 = new Book("My Dog will Deplete my Rocket Launcher Stash", "Ada Wong", 322, 2002);
 
-
-			Book[] books;
-			{
-				Book book1 = new("Harbinger of Chaos: Ruination", "Amy Stake", 911, 2001);
-				Book book2 = new("Lucy was in Fact a Lucifer", "Paige Turner", 666, 1313);
-				Book book3 = new("Shack Pallett at 5", "Jenny Rator", 614, 2016);
-				Book book4 = new("How Backflips are Relevant during Fight Scenes", "Leon S. Kennedy", 322, 1996);
-				Book book5 = new("My Dog will Deplete my Rocket Launcher Stash", "Ada Wong", 322, 2002);
-			};
+			Book[] books = new Book[] { book1, book2, book3, book4, book5 };
 
 			
 
